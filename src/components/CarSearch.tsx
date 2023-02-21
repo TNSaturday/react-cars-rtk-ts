bd28dc0fc26e296a5bc9d6aea3b89c4706bd6919
TNSaturday <tnsaturday@gmail.com>
2023-02-21 09:44:16 +0300

    Connect CarSearch to store

--- src/components/CarSearch.tsx
+++ src/components/CarSearch.tsx
@@ -1,5 +1,32 @@
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { changeSearchTerm } from "../store";
+
 function CarSearch() {
-  return <div>CarSearch</div>;
+  const { searchTerm } = useAppSelector((state) => state.cars);
+  const dispatch = useAppDispatch();
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label htmlFor="searchInput" className="label">
+          Search
+        </label>
+        <input
+          id="searchInput"
+          className="input"
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CarSearch;
