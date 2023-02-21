780e3a2dbd7da1b74c029408db77b32ba998f360
TNSaturday <tnsaturday@gmail.com>
2023-02-21 07:17:18 +0300

    Fix heading

--- src/components/CarForm.tsx
+++ src/components/CarForm.tsx
@@ -12,24 +12,23 @@ function CarForm() {
 
   return (
     <div className="car-form panel">
-      <h4 className="subtitle is-3">
-        <form>
-          <div className="field-group">
-            <div className="field">
-              <label htmlFor="carName" className="label">
-                Name
-              </label>
-              <input
-                id="carName"
-                type="text"
-                className="input is-expanded"
-                value={name}
-                onChange={handleNameChange}
-              />
-            </div>
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label htmlFor="carName" className="label">
+              Name
+            </label>
+            <input
+              id="carName"
+              type="text"
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
           </div>
-        </form>
-      </h4>
+        </div>
+      </form>
     </div>
   );
 }
