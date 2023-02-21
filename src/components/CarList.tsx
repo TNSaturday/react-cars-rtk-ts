839f44fa64a2a01a911f93a2f725db8024a9920a
TNSaturday <tnsaturday@gmail.com>
2023-02-21 10:32:15 +0300

    Add car highlightning functionality to CarList

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -4,25 +4,36 @@ import { removeCar } from "../store";
 function CarList() {
   const dispatch = useAppDispatch();
   const handleDeleteCar = (id: string) => dispatch(removeCar(id));
-  const cars = useAppSelector(({ cars: { cars, searchTerm } }) =>
-    cars.filter((car) =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+  const { cars, name } = useAppSelector(
+    ({ form, cars: { cars, searchTerm } }) => {
+      const filteredCars = cars.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      return {
+        cars: filteredCars,
+        name: form.name,
+      };
+    }
   );
 
-  const renderedCars = cars.map((car) => (
-    <div key={car.id} className="panel">
-      <p>
-        {car.name} - ${car.cost}
-      </p>
-      <button
-        className="button is-danger"
-        onClick={() => handleDeleteCar(car.id)}
-      >
-        Delete
-      </button>
-    </div>
-  ));
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={car.id} className={`panel${bold ? " bold" : ""}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleDeleteCar(car.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
   return <div className="car-list">{renderedCars}</div>;
 }
 
